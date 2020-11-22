a1c6baa2398531e045761463a80373f98329620f
sonam <sonam.tmg96@gmail.com>
2020-11-22 14:14:01 -0500

    removed smelicon

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ import torch
 #%%
 
 #load total listings from csv file onto pd object
-data = pd.read_csv('listings.csv');
+data = pd.read_csv('listings.csv')
 
 #initial data information and data separation
-data.info();
-X_pd = data.drop('price', axis=1);
-y_pd = data['price'];
+data.info()
+X_pd = data.drop('price', axis=1)
+y_pd = data['price']
 
 #pd object conversion to np for easy data manipulation
-X_np = X_pd.to_numpy();
-y_np = y_pd.to_numpy();
+X_np = X_pd.to_numpy()
+y_np = y_pd.to_numpy()
 
 #%%
 
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_np, y_np, test_size=0.30, random_state=0);
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_np, y_np, test_size=0.30, random_state=0)
