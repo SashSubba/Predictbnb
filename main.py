017f6cc1f2e1efa5e17e3fd414c63b6680df29ac
Sashank Subba <s_ubba@encs.concordia.ca>
2020-12-04 15:59:48 -0500

    [feature_selection] minor issue fixes.

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import sklearn
 import sklearn.linear_model
 import sklearn.model_selection
 import torch
-
+from sklearn.metrics import mean_absolute_error
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import mutual_info_regression
-#%%
 
+#%%
 X = prep.X_pd
 y = prep.y_pd.astype('string').apply(lambda x: x.str.strip('$')).apply(lambda x: x.str.replace(',', '')).astype('float')
 
@@ -21,15 +21,13 @@ y_np = y.to_numpy()
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_np, y_np, test_size=0.30, random_state=0)
 
 #%% Feature Selection
-fs = SelectKBest(score_func=mutual_info_regression, k='all')
+fs = SelectKBest(score_func=mutual_info_regression, k=50)
 fs.fit(X_train, y_train)
 X_train_fs = fs.transform(X_train)
 X_test_fs = fs.transform(X_test)
+
 for i in range(len(fs.scores_)):
 	print('Feature %d: %f' % (i, fs.scores_[i]))
 
-#%%
-linear_model = sklearn.linear_model.LinearRegression()
-linear_model.fit(X, y)
-
-# %%
+linear_model = sklearn.linear_model.LinearRegression(fit_intercept=False)
+print("training accuracy: " + str(linear_model.fit(X_train_fs,y_train).score(X_train_fs,y_train)*100) + "%")
